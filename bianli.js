5644dfaeb79cf84a516910c87335137f4fdd9a56
xiaosongye <846083469@qq.com>
2019-12-26 22:08:17 +0800

    数组去重排序

--- bianli.js
+++ bianli.js
@@ -5,5 +5,7 @@ let arr1 = "name";
 // arr1.forEach(item => {
 //   console.log(item);
 // });
-let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2];
+let arr2 = [1, 2, [5, 2, 3], [[[3], 4, 5], 6], 7, 8, 1, 2];
+arr2 = Object.values([...new Set(arr2.flat(Infinity))]);
+console.log(arr2);
 arr2.forEach(item => console.log(item));
