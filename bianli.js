a8e5574a649d7d71345ee7d5764bb6124a0349a8
xiaosongye <846083469@qq.com>
2019-12-26 15:36:33 +0800

    遍历

---
+++ bianli.js
@@ -0,0 +1,9 @@
+let arr1 = "name";
+// for (var i = 0; i < arr1.length; i++) {
+//   console.log(arr1[i]);
+// }
+// arr1.forEach(item => {
+//   console.log(item);
+// });
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2];
+arr2.forEach(item => console.log(item));
