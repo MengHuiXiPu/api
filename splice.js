34cf779c707b10867948ce215b9025dd7115466b
xiaosongye <846083469@qq.com>
2019-12-26 14:29:22 +0800

    split用法

---
+++ splice.js
@@ -0,0 +1,17 @@
+var pig = "river";
+var pigs = "river";
+// pig = pig.substr(1, 2);/
+pig = pig.substring(1, 2);
+pigs = pigs.substr(1, 2);
+// pig = pig.split("");
+// pigs = pigs.split("");
+// console.log(pig);
+// pig = pig.slice(1);
+// console.log(pig);
+// pig = pig.slice(-1);
+// console.log(pig);
+// pig = pig.splice(1,2);
+// pig = pig.slice(-3);
+// pigs = pigs.splice(-3);
+console.log(pig);
+console.log(pigs);
